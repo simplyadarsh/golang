fe29e0dca27e264da71302c76c8e090865bf2325
Adarsh Verma <64259125+simplyadarsh@users.noreply.github.com>
2024-09-16 20:12:54 +0530

    Demostrated format specifier

---
+++ data-types and variables/format_specifier.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt")
+
+func main() {
+	var(
+		city string = "Delhi"
+		country string = "India"
+		code int = 91
+	)
+	fmt.Printf("%s, is a capital of %s whose country code is %d",city,country,code)
+}
